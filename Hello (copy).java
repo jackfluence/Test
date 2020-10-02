6f40311e24e59bcd4c5b7b6d8b507a9ac0230973
jackfluence <jackfluence@gmail.com>
2020-10-02 14:22:12 +0530

    This is a test workspace

--- Hello (copy).java
+++ Hello (copy).java
@@ -2,4 +2,5 @@ public static void main(String args[])
 {
 System.out.println("This is a hello world program");
   //Test addition
+This is a test addition
 }
---
+++ Test/src/com/jackson/main/Testmain.java
@@ -0,0 +1,10 @@
+package com.jackson.main;
+
+public class Testmain {
+public static void main(String args[]) {
+	
+	System.out.println("This is a test class");
+	
+	
+}
+}
